a0a61d96fe3231fde6c07cd9e1b567aff4a64add
liuzhiyao <lzy_ok@aliyun.com>
2018-06-20 00:46:14 +0800

    添加详情页banner

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg" alt="">
+    <div class="banner-info">
+      <div class="banner-title">上海迪士尼乐园</div>
+      <div class="banner-number"><span class="iconfont banner-icon ">&#xe692;</span> 18</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #ffffff
+      background-image linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        height .32rem
+        line-height .32rem
+        padding 0 .4rem
+        margin-top .14rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+</style>
