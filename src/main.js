5e08f7b10520d8a9cc51aa1bc75e96e59f27e071
刘智尧(LIUZHIYAO837) <liuzhiyao837@pingan.com.cn>
2020-06-20 20:19:16 +0800

    fix:修复问题

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
+/*
+ * @Description: main.js
+ * @Author: Looper
+ * @Date: 2020-06-20 20:05:30
+ * @LastEditors: Looper
+ * @LastEditTime: 2020-06-20 20:11:02
+ * @FilePath: /Travel/src/main.js
+ */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './store'
 
@@ -15,7 +22,6 @@ import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
-fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
