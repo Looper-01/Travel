c22571d25f72016cfb6b9894ebbbbaa25229a8a8
Looper <lzy_ok@aliyun.com>
2020-06-27 22:39:14 +0800

    [update] 继续优化Home.vue

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,13 +3,13 @@
  * @Author: Looper
  * @Date: 2020-06-20 20:20:43
  * @LastEditors: Looper
- * @LastEditTime: 2020-06-26 15:35:19
+ * @LastEditTime: 2020-06-27 22:36:29
  * @FilePath: /Travel/src/pages/home/Home.vue
 --> 
 <template>
   <div>
     <home-header />
-    <home-swiper :list="data.swiperList" />
+    <home-swiper :list="swiperList" />
     <home-icons :list="data.iconList" />
     <home-recommend :list="data.recommendList" />
     <home-weekend :list="data.weekendList" />
@@ -24,7 +24,7 @@ import HomeRecommend from "./components/Recommend";
 import HomeWeekend from "./components/Weekend";
 import axios from "axios";
 import { useStore } from "vuex";
-import { reactive, computed, onMounted, onActivated } from "vue";
+import { reactive, computed, onMounted, onActivated, ref } from "vue";
 export default {
   name: "Home",
   components: {
@@ -35,37 +35,34 @@ export default {
     HomeWeekend
   },
   setup() {
-    // 原data函数，需要reactive函数封装才可以响应式
+    // 方法1：原data函数，需要reactive函数封装才可以响应式
     const data = reactive({
       lastCity: "",
-      swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     });
 
+    // 方法2：使用ref来包装数据，实现响应式，底层原理是通过proxy来实现劫持
+    const swiperList = ref([]);
+
     // vuex
     const store = useStore();
 
     // computed属性
-    const city = computed(() => {
-      return store.state.city;
-    });
+    const city = store.state.city;
 
     // methods start
-    function getHomeInfoSucc(res) {
+    async function getHomeInfo() {
+      let res = await axios.get("/api/index.json?city=" + city);
       res = res.data;
       if (res.ret && res.data) {
         const result = res.data;
-        data.swiperList = result.swiperList;
+        swiperList.value = result.swiperList;
         data.iconList = result.iconList;
         data.recommendList = result.recommendList;
         data.weekendList = result.weekendList;
       }
-    }
-
-    function getHomeInfo() {
-      axios.get("/api/index.json?city=" + city).then(getHomeInfoSucc);
     }
     // methods end
 
@@ -83,7 +80,7 @@ export default {
       }
     });
     // 输出
-    return { data, city };
+    return { data, city, swiperList };
   }
 };
 </script>
